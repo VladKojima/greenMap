c9855050e32ee314dcc210ca75499a68252dc452
VladKojima <102826875+VladKojima@users.noreply.github.com>
2022-12-24 13:41:32 +0400

    картинка по умолчанию в кружке

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -3,8 +3,6 @@ let img = card.querySelector('.card .top .img');
 let coords = card.getElementsByClassName('coordinates')[0];
 let h5_name = card.querySelector('h5 span.name');
 
-img.onerror=()=>setDefImg(img)
-
 let index_tree = -1;
 
 let dark = document.getElementsByClassName('dark')[0];
@@ -13,7 +11,7 @@ let photo = dark.getElementsByTagName('img')[0];
 function open_card(index) {
 
     index_tree = index;
-    img.setAttribute("style", "background-image: url('../assets/images/"+datas[index]['photo']+"')");
+    img.setAttribute("style", "background-image: url('../assets/images/"+datas[index]['photo']+"'), url('../assets/images/testicon.png')");
     h5_name.innerHTML = datas[index]['name'];
     coords.innerHTML = datas[index]['coordinates'][0] + ", " + datas[index]['coordinates'][1];
     card.querySelector('[class=number]').innerHTML = datas[index]['id'];
